a84dac7ed76d97620a601a13ec77aa19d3b2161b
Paskal <paskal.arzel@gmail.com>
2024-01-23 17:36:50 +0100

    Poisonous arrow done

--- Projectiles/PoisonousArrow.cs
+++ Projectiles/PoisonousArrow.cs
@@ -11,23 +11,22 @@ using Elementals.Content.Buffs;
 
 namespace Elementals.Projectiles
 {
-	// This projectile showcases advanced AI code. Of particular note is a showcase on how projectiles can stick to NPCs in a manner similar to the behavior of vanilla weapons such as Bone Javelin, Daybreak, Blood Butcherer, Stardust Cell Minion, and Tentacle Spike. This code is modeled closely after Bone Javelin.
 	public class PoisonousArrow : ModProjectile
 	{
 		public override void SetDefaults() {
-			Projectile.width = 16; // The width of projectile hitbox
-			Projectile.height = 16; // The height of projectile hitbox
-			Projectile.aiStyle = 0; // The ai style of the projectile (0 means custom AI). For more please reference the source code of Terraria
-			Projectile.friendly = true; // Can the projectile deal damage to enemies?
-			Projectile.hostile = false; // Can the projectile deal damage to the player?
-			Projectile.DamageType = DamageClass.Ranged; // Makes the projectile deal ranged damage. You can set in to DamageClass.Throwing, but that is not used by any vanilla items
-			Projectile.penetrate = 2; // How many monsters the projectile can penetrate.
-			Projectile.timeLeft = 600; // The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
-			Projectile.alpha = 255; // The transparency of the projectile, 255 for completely transparent. Our custom AI below fades our projectile in. Make sure to delete this if you aren't using an aiStyle that fades in.
-			Projectile.light = 0.5f; // How much light emit around the projectile
-			Projectile.ignoreWater = true; // Does the projectile's speed be influenced by water?
-			Projectile.tileCollide = true; // Can the projectile collide with tiles?
-			Projectile.hide = true; // Makes the projectile completely invisible. We need this to draw our projectile behind enemies/tiles in DrawBehind()
+			Projectile.width = 16;
+			Projectile.height = 16;
+			Projectile.aiStyle = 1;
+			Projectile.friendly = true;
+			Projectile.hostile = false;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.timeLeft = 600;
+			Projectile.alpha = 0;
+			Projectile.light = 0;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = true;
+			Projectile.extraUpdates = 1;
+			AIType = ProjectileID.WoodenArrowFriendly;
 		}
 
         public override bool PreDraw(ref Color lightColor) {
