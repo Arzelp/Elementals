4185bb98e64f9421295c5df7aeea1f5486d14482
Paskal <paskal.arzel@gmail.com>
2024-02-12 14:51:03 +0100

    Amber Powder

---
+++ Content/Items/Components/AmberPowder.cs
@@ -0,0 +1,23 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+
+namespace Elementals.Content.Items.Components
+{
+    public class AmethystPowder : ModItem
+    {
+        public override void SetDefaults() {
+            Item.width = 10;
+            Item.height = 10;
+            Item.maxStack = 999;
+            Item.value = Item.sellPrice(silver: 2, copper: 50);
+        }
+
+        public override void AddRecipes() {
+            Recipe recipe = CreateRecipe(10);
+            recipe.AddIngredient(999, 1);
+            recipe.Register();
+        }
+    }
+}
